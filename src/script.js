f2144c70439ae531da318db2336177b9dcae1bc0
Moses-Alero <mosesalero@gmail.com>
2022-04-13 14:15:05 +0100

    Merge branch 'dev' of https://github.com/Moses-Alero/Final-Year-Project into dev

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const AES  =  require('./Aes');
+const AES  =  require('./utils/Aes');
 const  { hash } = require('./utils/hash');
 
 const textToEncode = "Linear Programming (LP) or Linear Optimization can be defined as a problem of maximizing or minimizing a linear function subject to linear constraints."
